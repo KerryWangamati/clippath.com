b9bad5557efd1cf01e84c277c687b203706a1138
KerryWangamati <91453490+KerryWangamati@users.noreply.github.com>
2021-10-22 12:56:37 +0300

    Added new

---
+++ canvas.js
@@ -0,0 +1,58 @@
+// const canvas=document.querySelector("canvas");
+
+// const ctx=canvas.getContext("2d");
+
+// canvas.width=window.innerWidth-400;
+// canvas.height=400;
+
+// Lines
+// ctx.moveTo(50,100);
+// ctx.lineTo(100,100);
+// ctx.stroke();
+
+// Triangle
+// ctx.moveTo(50,50);
+// ctx.lineTo(150,50);
+// ctx.lineTo(100,200);
+// ctx.closePath();
+// // ctx.stroke();
+// ctx.fillStyle="coral";
+// ctx.fill()
+
+
+// ctx.moveTo(250,50);
+// ctx.lineTo(350,50);
+// ctx.lineTo(300,200);
+// ctx.closePath();
+// ctx.stroke();
+
+// Rectangle
+// ctx.fillStyle="coral"
+// ctx.fillRect(50,50, 100,50);
+
+// ctx.lineTo(50,50);
+// ctx.rect(50,50,400,90);
+// ctx.fillStyle="coral";
+// ctx.fill();
+
+
+// Circles
+// ctx.beginPath();
+// ctx.arc(canvas.width/2,canvas.height/2,50,0,Math.PI*2);
+// // ctx.arc(canvas.width/2,canvas.height/2,50,0,Math.PI, false);
+// ctx.fillStyle="coral"
+// ctx.fill();
+
+// Quadratic curves
+// ctx.moveTo(10,10);
+// ctx.quadraticCurveTo(20,45,15,9);
+// ctx.quadraticCurveTo(21,55,25,17);
+// ctx.quadraticCurveTo(30,90,43,40);
+// ctx.quadraticCurveTo(70,25,55,60);
+// ctx.stroke();
+
+// ctx.moveTo(10,10);
+// ctx.bezierCurveTo(15,15,20,25,30,35);
+// ctx.bezierCurveTo(25,25,30,35,40,45);
+// ctx.bezierCurveTo(35,35,40,45,50,55);
+// ctx.stroke();
